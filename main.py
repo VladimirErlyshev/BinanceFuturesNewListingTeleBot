6cd974bb21cc37f767cb5434d31bc56f565ff8b6
Vladimir Erlyshev <vladimirerl@gmail.com>
2024-04-09 01:02:36 +0500

    add function get_futures_list

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print('test')
+import requests
+from application_info import BINANCE_API_URL
+
+
+def get_futures_ticker_list():
+    response = requests.get(BINANCE_API_URL)
+    md = response.json()
+    symbols = md['symbols']
+    futures_list = []
+    for symbol in symbols:
+        if symbol['isMarginTradingAllowed'] and 'USDT' in symbol['symbol']:
+            futures_list.append(symbol['baseAsset'])
+    return futures_list
+
+
+if __name__ == '__main__':
+    print(get_futures_ticker_list())
+    print(len(get_futures_ticker_list()))
