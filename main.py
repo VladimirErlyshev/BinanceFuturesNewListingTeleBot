036a5de096d284eece1d1bd1d165270751bcf52b
Vladimir Erlyshev <vladimirerl@gmail.com>
2024-04-09 19:34:57 +0500

    add function get_futures_list

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ def get_futures_ticker_list() -> list:
     for symbol in symbols:
         if symbol['isMarginTradingAllowed'] and ('USDT' in symbol['symbol'] or 'USDC' in symbol['symbol']):
             futures_list.append(symbol['baseAsset'])
+
     return futures_list
 
 
@@ -28,7 +29,7 @@ def open_file_with_futures_tickers() -> list:
     with open('files/tickers.csv', 'r') as file:
         csv_dict_reader = csv.DictReader(file)
         for row in csv_dict_reader:
-            tickers_list.append(row['ticker'])
+            tickers_list.append(row["ticker"])
 
     return tickers_list
 
@@ -36,19 +37,22 @@ def open_file_with_futures_tickers() -> list:
 def detected_new_futures() -> str:
     set_for_api = set(get_futures_ticker_list())
     set_for_file = set(open_file_with_futures_tickers())
-    new_set = {}
     if set_for_api == set_for_file:
-        return 'Изменений апи бинанса не обнаружено'
+
+        return 'Изменений в апи бинанса не обнаружено'
+
     elif len(set_for_api) > len(set_for_file):
         new_set = set_for_api.difference(set_for_file)
         save_futures_list_to_file()
+
         return f'Новые фьючерсы в апи бинанса {new_set}'
+
     else:
         new_set = set_for_file.difference(set_for_api)
         save_futures_list_to_file()
+
         return f'Отсутствуют фьючерсы в апи бинанса {new_set}'
 
 
 if __name__ == '__main__':
     print(detected_new_futures())
-
